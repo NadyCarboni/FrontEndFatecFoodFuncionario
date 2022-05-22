f792b6a06ca23f1ab7f4c477b3fafad40159bdac
NadyCarboni <nady.n.m.c@gmail.com>
2022-05-22 17:21:04 -0300

    Fila de pedidos

--- src/Pages/FilaPedidos/Componentes/pedido.tsx
+++ src/Pages/FilaPedidos/Componentes/pedido.tsx
@@ -20,14 +20,45 @@ export default function Pedido({ comanda, data, id, itemSelecionado }: any) {
   };
 
   return (
-    <div className="pedido m-3 flex py-4">
-      <p>
-        <span>Pedido {id}</span>
+    <div className="pedido m-3 flex py-4 column">
+      <p className="mx-4">
+        <span className="numeroPedido poppins small">Pedido {id}</span>
         <span className=" mx-2 titleGrad1 px-2 comanda">
           Comanda: {comanda}
         </span>
         <span>Data: {getDate(data)}</span>
       </p>
+      <div className=" itemLista flex m-4">
+        <div className="itemTitulo p-2">Itens selecionados:</div>
+        <ul>
+          {itemSelecionado?.map((element: any) => {
+            return (
+              <li className="item-selecionados ">
+                <ul className="m-2 flex column flex py-2">
+                  <li>
+                    <span className="titleGrad1 small poppins mx-2 px-1">
+                      Observacões:{" "}
+                    </span>
+                    {element.observacoes}
+                  </li>
+                  <li>
+                    <span className="titleGrad1 small poppins mx-2 px-1">
+                      Produto:{" "}
+                    </span>
+                    {element.produtoId}
+                  </li>
+                  <li>
+                    <span className="titleGrad1 small poppins mx-2 px-1">
+                      Quantidade:{" "}
+                    </span>
+                    {element.quantidade}
+                  </li>
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       {/* <p>{itemSelecionado}</p> */}
     </div>
   );
